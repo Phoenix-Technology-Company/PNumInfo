019bd6dd43bfd4b7e54878dbce8412ab0c5c07cd
Harry <78296890+phoenix-ph@users.noreply.github.com>
2021-07-11 12:27:08 +0430

    Add files via upload

---
+++ Test/test_tur.py
@@ -0,0 +1,5 @@
+from PNumInfo import tur
+
+tur.info("+90382876523",True) 
+tur.info("+90382876523")
+print(tur.info("+90382876523"))
---
+++ Test/test_usa.py
@@ -0,0 +1,5 @@
+from PNumInfo import usa
+
+usa.info("+12108765239",True) 
+usa.info("+12108765239")
+print(usa.info("+12108765239"))
